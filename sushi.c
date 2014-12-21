1ede43c73f2163144a70e31394c2a0fa5968f21d
riekohirota <crista.p.p.j@gmail.com>
2014-12-22 01:26:07 +0900

    game over画面の問題を修正

--- sushi.c
+++ sushi.c
@@ -90,7 +90,10 @@ struct sushi_ctx *sushi_init(char const *sushi, char const *fish[], int len, dou
 int sushi_show_game_over(struct sushi_ctx *sc)
 {
 	int score = sc->sushi_len;
+
+	usleep(500000);
 	erase();
+	refresh();
 	/* ここにゲームオーバーの画面を表示 */
 	printf("\033[5;0H"); 
 	printf("\t*** Game Over --- Score:%d貫 ***\n", score);
